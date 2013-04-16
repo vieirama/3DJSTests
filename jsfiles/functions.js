d7103b9746dd7812a78b18a89e4b041221d599d7
miguel.vieira <miguel.vieira@cision.com>
2013-04-16 10:13:26 +0100

    First Commit

---
+++ jsfiles/functions.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+
+    $("#submit").die("click").live("click", function () {
+        callPost();
+    });
+
+    function callPost() {
+        $("#warning").text("");
+        $("#warning").hide();
+        var json = $("#jsonString").val();
+        
+        if (json == "") {
+            $("#warning").text("String can not be empty");
+            $("#warning").show();
+        }
+
+
+        //Insert code here...
+    }
+
+
+});
