c4bbad12efbe77e501e9cb8b03e2d58a1a054ae8
tiago.nunes <tiago.nunes@cision.com>
2013-04-18 11:21:50 +0100

    Line chart with dates

--- jsfiles/LineChartFunctions.js
+++ jsfiles/LineChartFunctions.js
@@ -21,21 +21,26 @@ $(document).ready(function () {
             console.log(jsonObject);
 
             var values = [];
-            var w = 400;
-            var h = 400;
+            var w = 1200;
+            var h = 300;
             var margin = 40;
             $(".bar").remove();
             $(".label").remove();
             $("svg").remove();
 
             data = jsonObject.dataset;
+            data.sort(function (a, b) { return d3.ascending(a.date, b.date); });
 
             var y = d3.scale.linear().domain([0, d3.max(data, function (d) { return d.value; })]).range([0 + margin, h - margin]),
             yInv = d3.scale.linear().domain([0, d3.max(data, function (d) { return d.value; })]).range([h - margin, 0 + margin]),
             //x = d3.scale.linear().domain([0, data.length - 1]).range([0 + margin, w - margin]);
             x = d3.time.scale()
-	            .domain([d3.min(data, function (d) { return Date(d.date); }), d3.max(data, function (d) { return d.date; })])    // values between for month of january
-		        .range([0 + margin, w - margin]);
+	            .domain([d3.min(data, function (d) {
+	                //var dt = new Date(d.date);
+	                return d.date*1000;
+	            }), d3.max(data, function (d) { return d.date * 1000; })])    // values between for month of january
+		        .range([0 + margin, w - margin])
+            //.ticks(d3.time.day, 15);
 
             var vis = d3.select("#graph")
 						.append("svg")
@@ -46,8 +51,9 @@ $(document).ready(function () {
             .attr("transform", "translate(0, " + (h) + ")");
 
             var line = d3.svg.line()
-            .x(function (d, i) { return x(i); })
+            .x(function (d, i) { return x(d.date * 1000); })
             .y(function (d) { return -1 * margin; });
+            //.y(function (d) { return -1 * y(d.value); });
 
             g.append("svg:path")
             .attr("d", line(data))
@@ -58,7 +64,8 @@ $(document).ready(function () {
             //AXIS
             var xAxis = d3.svg.axis()
 			                .scale(x)
-            //.ticks(5)
+    //.ticks(d3.time.months, 1)
+    .tickFormat(d3.time.format('%m %Y'))
 			                .orient("bottom");
             vis.append("g")
 			.attr("class", "axis")
@@ -73,10 +80,10 @@ $(document).ready(function () {
 			.attr("transform", "translate(" + margin + ",0)")
 			.call(yAxis);
 
-            line = d3.svg.line()
-            .x(function (d, i) { return x(i); })
-            .y(function (d) { return -1 * y(d.value); });
-            g.selectAll("path").transition().ease("").duration(2000).attr("d", line(data))
+                        line = d3.svg.line()
+                        .x(function (d, i) { return x(d.date * 1000); })
+                        .y(function (d) { return -1 * y(d.value); });
+                        g.selectAll("path").transition().ease("").duration(2000).attr("d", line(data))
 
         }
     }
@@ -108,7 +115,7 @@ $(document).ready(function () {
 
             var y = d3.scale.linear().domain([0, d3.max(data)]).range([0 + margin, h - margin]),
             yInv = d3.scale.linear().domain([0, d3.max(data)]).range([h - margin, 0 + margin]),
-            x = d3.scale.linear().domain([0, data.length-1]).range([0 + margin, w-margin]);
+            x = d3.scale.linear().domain([0, data.length - 1]).range([0 + margin, w - margin]);
 
 
             var vis = d3.select("#graph")
