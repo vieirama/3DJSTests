ca4d794dbfd3fba9e140fbc7ed52c40c11a2162c
Henrique Guerra <henriqueguerra1@hotmail.com>
2013-04-22 12:22:28 +0100

    Added tooltips, added legend, added circle grow on hover

--- jsfiles/LineChartFunctions.js
+++ jsfiles/LineChartFunctions.js
@@ -29,8 +29,8 @@ $(document).ready(function () {
 
             var values = [];
             var w = 960;
-            var h = 600;
-            var margin = 130;
+            var h = 800;
+            var margin = 180;
             $(".bar").remove();
             $(".label").remove();
             $("svg").remove();
@@ -46,19 +46,21 @@ $(document).ready(function () {
             yInv = d3.scale.linear().domain([0, d3.max(data, function (d) { return d.Y; })]).range([h - margin, 0 + margin]),
             //x = d3.scale.linear().domain([0, data.length - 1]).range([0 + margin, w - margin]);
             x = d3.scale.ordinal().rangeRoundBands([0 + margin, w - margin])
-	            .domain(data.map(function (d) { return d.X; }));   
+	            .domain(data.map(function (d) { return d.X; }));
 
             var vis = d3.select("#graph")
 						.append("svg")
                         .attr("width", w)
 						.attr("height", h);
 
+            var formatTime = d3.time.format("%m-%d-%Y");
+
 
             //AXIS
             var xAxis = d3.svg.axis()
 			                .scale(x)
             //.ticks(24)
-                            .tickFormat(d3.time.format('%m-%d-%Y'))
+                            .tickFormat(formatTime)
                             .tickSize(0, 0, 3)
 			                .orient("bottom");
 
@@ -67,15 +69,16 @@ $(document).ready(function () {
 			.attr("transform", "translate(0," + (h - margin) + ")")
 			.call(xAxis)
             .append("text")
-            .attr("transform", "translate(" + ((w - margin) / 2) + "," + margin + ")")
+            .attr("transform", "translate(430,130)")
             .text("Received Date"); //Has to be passed on JSON
-            
-            vis.selectAll(".tick text")
-            .attr("transform", function (d) { return "translate(" + this.getBBox().height * -1.5 + "," + (this.getBBox().height + 40) + ")rotate(-90)"; })
+
+            var AxisXtext = vis.selectAll(".tick text")
+            .attr("transform", function (d) { return "translate(" + this.getBBox().height * -1 + "," + (this.getBBox().height + 40) + ")rotate(-90)"; })
             .on("mouseover", function (d, i) {
                 d3.select(points[0][i]).transition()
                             .duration(300)
-                            .attr("r", 7);             
+                            .attr("r", 7)
+                            .attr("fill", "red");
                 d3.select(this).transition().ease("").duration(200)
                             .style("font-size", "20px");
             })
@@ -83,24 +86,25 @@ $(document).ready(function () {
                 d3.select(points[0][i]).transition()
                     .ease("elastic")
                     .duration(600)
-                    .attr("r", 4);
+                    .attr("r", 4)
+                    .attr("fill", "black");
                 d3.select(this).transition().ease("").duration(200)
-                .style("font-size", "15px");
+                .style("font-size", "16px");
             });
 
             var yAxis = d3.svg.axis()
                             .tickSize(-w + 2 * margin, 50, 3)
 			                .scale(yInv)
 			                .orient("left");
-                            
+
             vis.append("g")
 			.attr("class", "y axis")
 			.attr("transform", "translate(" + margin + ",0)")
 			.call(yAxis)
             .append("text")
-            .attr("transform", "translate(" + (-margin / 2) + "," + ((h + margin) / 2) + ")rotate(-90)")
+            .attr("transform", "translate(-60," + ((h + margin - 50) / 2) + ")rotate(-90)")
             .text("Number of Articles");  //Has to be passed on JSON
-            
+
             var g = vis.append("svg:g")
             .attr("transform", "translate(0, " + (h) + ")");
 
@@ -111,9 +115,55 @@ $(document).ready(function () {
 
             g.append("svg:path")
             .attr("d", line(data))
-            .attr("stroke", "red")
+            .attr("stroke", "blue")
             .attr("stroke-width", "2")
             .attr("fill", "none");
+
+            //Title
+            var title = vis.append("text")
+            .attr("class", "legend")
+            .attr("height", 100)
+            .attr("width", 100)
+            .text("Frequency Trend") //NEEDS TO BE ADDED TO JSON
+            .attr("x", w / 2)
+            .attr("y", 70)
+            .attr("text-anchor", "middle")
+            .attr("font-size", "36px");
+
+            //SubTitle
+            var subTitle = vis.append("text")
+            .attr("class", "legend")
+            .attr("height", 100)
+            .attr("width", 100)
+            .text("Grouped by Subscriptions") //NEEDS TO BE ADDED TO JSON
+            .attr("x", w / 2)
+            .attr("y", 100)
+            .attr("text-anchor", "middle")
+            .attr("font-size", "20px");
+
+            var legendline = vis.append("line")
+            .attr("x1", w / 2 - 70)
+            .attr("y1", 780)
+            .attr("x2", w / 2 - 50)
+            .attr("y2", 780)
+            .attr("stroke-width", 2)
+            .attr("stroke", "blue");
+
+            var legendText = vis.append("text")
+            .attr("class", "legend")
+            .attr("height", 100)
+            .attr("width", 100)
+            .text("Number of Articles") //NEEDS TO BE ADDED TO JSON
+            .attr("x", w / 2 - 40)
+            .attr("y", 784)
+            .attr("text-anchor", "left")
+            .attr("font-size", "13px");
+
+            //Add tooltip
+
+            var div = d3.select("body").append("div")
+                        .attr("class", "tooltip")
+                        .style("opacity", 0);
 
             var points = g.selectAll('circle.mark').data(data).enter().append('svg:circle')
             .attr('class', 'mark')
@@ -121,8 +171,36 @@ $(document).ready(function () {
             .attr('cy', function (d) { return -1 * margin; })
             .attr('r', 4)
             .on("click", function (d, i) {
-                alert("ID to link is: " + data[i].XId);
+                alert("ID to link is: " + d.XId);
             })
+            .on("mouseover", function (d, i) {
+                div.transition()
+                .duration(200)
+                .style("opacity", .9);
+                div.html(formatTime(d.X) + "<br/>" + d.Y)
+                .style("left", (d3.event.pageX + 5) + "px")
+                .style("top", (d3.event.pageY - 32) + "px");
+
+                d3.select(this).transition()
+                            .duration(300)
+                            .attr("r", 7)
+                            .attr("fill", "red");
+                d3.select(AxisXtext[0][i]).transition().ease("").duration(200)
+                            .style("font-size", "20px");
+            })
+            .on("mouseout", function (d) {
+                div.transition()
+                    .duration(500)
+                    .style("opacity", 0);
+
+                d3.select(this).transition()
+                    .ease("elastic")
+                    .duration(600)
+                    .attr("r", 4)
+                    .attr("fill", "black");
+                AxisXtext.transition().ease("").duration(200)
+                .style("font-size", "16px");
+            });
 
 
             line = d3.svg.line()
@@ -134,7 +212,7 @@ $(document).ready(function () {
 
             points.transition().ease("").duration(2000)
             .attr('cx', function (d, i) { return x(d.X); })
-            .attr('cy', function (d) { return -1 * y(d.Y); })
+            .attr('cy', function (d) { return -1 * y(d.Y); });
 
         }
     }
