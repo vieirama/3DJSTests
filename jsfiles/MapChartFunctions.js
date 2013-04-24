e34a5c6d4fbe2893fac6f896c9796e2021abd73b
tiago.nunes <tiago.nunes@cision.com>
2013-04-24 16:01:42 +0100

    Europe map

--- jsfiles/MapChartFunctions.js
+++ jsfiles/MapChartFunctions.js
@@ -25,15 +25,21 @@ $(document).ready(function () {
 function drawMapChart(data){
   var formatTime = d3.time.format("%e %B");
 
+
   var width = 960,
       height = 500,
       centered;
  div = d3.select("#graph").append("div") 
-      .attr("class", "tooltip")               
+      .attr("class", "tooltip")
       .style("opacity", 0);
 
-  var path = d3.geo.path();
-   
+ var projection = d3.geo.mercator()
+//    .translate([height / 4, width/4])
+//    .scale(width / 2);
+ var path = d3.geo.path()
+    .projection(projection)
+    ;
+    
   var svg = d3.select("#graph").append("svg")
       .attr("width", width)
       .attr("height", height);
@@ -43,17 +49,19 @@ function drawMapChart(data){
       .attr("width", width)
       .attr("height", height);
       //.on("click", click);
-   
+
   var g = svg.append("g")
+  //.attr("transform", "translate(" + (width / 2-200) + "," + height / 2 + ")scale(1.7,1.2)") //Canada 
+  //.attr("transform", "translate(" + (-800) + "," + 50 + ")scale(2.5,2.5)") //Europe
       .attr("id", "states");
-     g.selectAll("path")
+     //g.selectAll("path")
         
   d3.json("readme.json", function(json) {
     g.selectAll("path")
         .data(json.features)
       .enter().append("path")
         .attr("d", path)
-  	  .attr("class",  function(d) { return (d.properties["name"] == "Illinois" ? "active" : ".features"); })
+  	  //.attr("class",  function(d) { return (d.properties["name"] == "Illinois" ? "active" : ".features"); })
         .on("click", click)
         .on("mouseover",hover );
 
@@ -72,7 +80,7 @@ function hover(d){
  div.transition()        
      .duration(200)      
      .style("opacity", .9);      
-  div .html(d.properties["name"])  
+  div .html(d.properties["NAME"])  
       .style("left", (d3.event.pageX) + "px")     
       .style("top", (d3.event.pageY - 28) + "px");                
 }
